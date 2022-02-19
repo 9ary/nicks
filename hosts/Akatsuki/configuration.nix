e045eea0022e22dc85547a4f793c1ac12a5ce9b8
novenary <streetwalkermc@gmail.com>
2022-02-19 19:34:25 +0200

    Yeet Plasma

--- hosts/Akatsuki/configuration.nix
+++ hosts/Akatsuki/configuration.nix
@@ -28,11 +28,6 @@
       keyMap = "us";
     };
 
-    services.xserver.enable = true;
-    services.xserver.displayManager.sddm.enable = true;
-    services.xserver.desktopManager.plasma5.enable = true;
-    services.xserver.layout = "us";
-
     sound.enable = true;
     hardware.pulseaudio.enable = true;
 

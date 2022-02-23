cdbb67fad6e803c4d0ad1367b06babe24cf7c052
novenary <streetwalkermc@gmail.com>
2022-02-23 09:47:48 +0200

    Enable pipewire

--- common/workstation/default.nix
+++ common/workstation/default.nix
@@ -2,8 +2,14 @@
 
 {
   config = lib.mkIf config.systemProfile.isWorkstation {
-    sound.enable = true;
-    hardware.pulseaudio.enable = true;
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
 
     nixpkgs.overlays = [
       (_: _: {

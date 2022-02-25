399959d8d59ab29494832baee0b201bb600d69b3
novenary <streetwalkermc@gmail.com>
2022-02-25 12:22:57 +0200

    Open port for Sonos volume control feedback

--- hosts/Akatsuki/configuration.nix
+++ hosts/Akatsuki/configuration.nix
@@ -33,6 +33,10 @@
       isWorkstation = true;
     };
 
+    # Sonos/SoCo
+    # TODO move Sonos volume control to a system module
+    networking.firewall.allowedTCPPorts = [ 1400 ];
+
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
     # on your system were taken. It‘s perfectly fine and recommended to leave

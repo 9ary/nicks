4c725ab8f6a7db6eb18cdd892fbe8bfed5b13391
novenary <streetwalkermc@gmail.com>
2022-03-06 22:52:20 +0200

    Fix ddcci udev rule

--- hosts/Akatsuki/ddcci.nix
+++ hosts/Akatsuki/ddcci.nix
@@ -3,11 +3,10 @@
 {
   config = {
     boot.extraModulePackages = [ config.boot.kernelPackages.ddcci-driver ];
-    boot.kernelModules = [ "ddcci" ];
     services.udev.extraRules = ''
       # Reload ddcci driver on monitor hotplug (since the driver doesn't handle it)
       SUBSYSTEM=="drm", ACTION=="change", \
-        RUN+="${pkgs.bash}/bin/sh -c 'rmmod ddcci_backlight ddcci; sleep 5; modprobe ddcci'"
+        RUN+="${pkgs.bash}/bin/sh -c '${pkgs.kmod}/bin/rmmod ddcci_backlight ddcci; sleep 5; ${pkgs.kmod}/bin/modprobe ddcci'"
     '';
   };
 }
